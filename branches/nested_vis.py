11430eb709e9b0011423a0af30276b4e45740330
Thuso Simon <thuso@ast.uct.ac.za>
2011-10-04 15:21:57 +0000

    completed viewing, made temp place for drawing elipses
    git-svn-id: https://agedate.googlecode.com/svn/ageDate@56 56a2626d-7c62-b8f8-b508-c160e9ae2a0b

--- branches/nested_vis.py
+++ branches/nested_vis.py
@@ -42,18 +42,55 @@ def elipse_vis(points,bins,N,elogf=1.1):
     #plot first N points
     fig=lab.figure()
     current=fig.add_subplot(111)
-    axis=current.plot(points[:N,0],points[:N,1],'o',markersize=5)
+    #plot chi squared landscape using griddata
+    metal,age=nu.linspace(metal_unq[0],metal_unq[-1],200),nu.linspace(age_unq[0],age_unq[-1],200)
+    z=lab.griddata(points[:,0],points[:,1],points[:,-1],metal,age)
+    current.pcolor(metal,age,nu.log10(z))
+    #lab.colorbar()
+    for i in xrange(N):
+        current.plot(points[i,0],points[i,1],'yo',markersize=5)
     current.set_xlim((metal_unq.min(),metal_unq.max()))
     current.set_ylim((age_unq.min(),age_unq.max()))
+    current.set_xlabel('Log Metalicity')
+    current.set_ylabel('Age')
     #plot current elipse
-    curren_ellipse=Ellipse(xy=rand(2)*10, width=rand(), height=rand())
-'''    ax.add_artist(e)
-    e.set_clip_box(ax.bbox)
-    e.set_alpha(rand())
-    e.set_facecolor(rand(3))
-    current.lines.pop
-'''
-                           
+    curren_ellipse=Ellipse(xy=[metal_unq[0]+metal_unq.ptp()/2.,
+                               age_unq[0]+age_unq.ptp()/2.], 
+                           width=metal_unq.ptp()*elogf,height=age_unq.ptp()*elogf)
+    current.add_artist(curren_ellipse)
+    curren_ellipse.set_alpha(.5)
+    #curren_ellipse.set_facecolor(nu.random.rand(3))
+    #current.lines.pop                         
     fig.canvas.show()
-    for i in xrange(N,points[N:,:].shape[0]): #add and remove ploints like in program
-        
+    for i in xrange(i,points[:,0].shape[0]): #add and remove ploints like in program
+        #remove last point and add new one in red
+        current.lines[i-N-1].set_visible(False)
+        #make last new point blue
+        current.lines[i-1].set_color('y')
+        current.plot(points[i,0],points[i,1],'ro',markersize=5)
+        #change ellipse size
+        current.artists.pop(0)
+        curren_ellipse=Ellipse(xy=nu.mean(points[i-N+1:i+1,:2],0),
+                                   width=points[i-N+1:i+1,0].ptp()*elogf,height=points[i-N+1:i+1,1].ptp()*elogf)
+        curren_ellipse.set_alpha(.5)
+        current.add_artist(curren_ellipse)
+        fig.canvas.show()
+
+def draw_ellipse():
+    s,t=nu.mat(nu.identity(2)),nu.mat(nu.identity(2))
+    X,Y=nu.linspace(-2.6,-2.25,200),nu.linspace(7.8,8.3,200)
+    x=nu.mat(nu.ones(2))
+    #points=[]
+    #for i in range(400):
+    #    points.append(nu.random.multivariate_normal([0,0],nu.array([[2,.342],[1,-.3242]])))
+    #points=nu.array(points)
+    cov=nu.cov(points.T)
+    s=nu.mat(cov)
+    mean=points.mean(0)
+    out=nu.zeros([200,200])
+    for i in range(200):
+        for j in range(200):
+            x[0,:2]=nu.array([X[i],Y[j]])-mean
+            if x*s**-1*x.T<=7:
+                out[i,j]=1
+    out=out.T
