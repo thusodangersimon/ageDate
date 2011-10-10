7f4f0fbb5a5fde666c1dae0acf8355598a82a8e7
Thuso Simon <thuso@ast.uct.ac.za>
2011-10-10 07:34:36 +0000

    added PMC to it
    git-svn-id: https://agedate.googlecode.com/svn/ageDate@57 56a2626d-7c62-b8f8-b508-c160e9ae2a0b

--- trunk/Age_MCMC.py
+++ trunk/Age_MCMC.py
@@ -13,6 +13,155 @@ def test():
     rank = comm.Get_rank()
     print 'i am %i' %rank
 
+def PMC(data,bins,n_dist=2,pop_num=10**4):
+    #uses population monte carlo to find best fits and prosteror
+    data[:,1]=data[:,1]*1000.  
+    cpu=float(cpu_count())
+    
+   #initalize parmeters and chi squared
+    lib_vals=get_fitting_info(lib_path)
+    lib_vals[0][:,0]=10**nu.log10(lib_vals[0][:,0]) #to keep roundoff error constistant
+    metal_unq=nu.log10(nu.unique(lib_vals[0][:,0]))
+    age_unq=nu.unique(lib_vals[0][:,1])
+    #initalize importance functions
+    alpha=nu.array([n_dist**-1.]*n_dist) #[U,N]
+    #for multivarate dist params
+    mu=nu.zeros([n_dist,bins*3])
+    age_bins=nu.linspace(age_unq.min(),age_unq.max(),bins+1)
+    for jj in range(n_dist):
+        bin_index=0
+        for k in xrange(mu.shape[1]):
+            if any(nu.array(range(0,mu.shape[1],3))==k):#metalicity
+                mu[jj,k]=(nu.random.random()*metal_unq.ptp()+metal_unq[0])
+            else:#age and normilization
+                if any(nu.array(range(1,mu.shape[1],3))==k): #age
+                #mu[k]=nu.random.random()
+                    mu[jj,k]=nu.random.rand()*age_unq.ptp()/float(bins)+age_bins[bin_index]
+               # mu[k]=nu.mean([bin[bin_index],bin[1+bin_index]])
+                    bin_index+=1
+                else: #norm
+                    mu[jj,k]=nu.random.random()
+    sigma=nu.array([nu.identity(bins*3)*nu.random.rand()/bins]*n_dist)
+    #build population parameters
+    points=pop_builder(pop_num,alpha,mu,sigma,age_unq,metal_unq,bins)
+    #get likelihoods
+    lik=[]
+    pool=Pool()
+    for ii in points:
+        pool.apply_async(like_gen,(data,ii,lib_vals,age_unq,metal_unq,bins,),callback=lik.append)
+    pool.close()
+    pool.join()
+    lik=nu.array(lik)
+    #calculate weights
+    for j in lik:
+        j[-1]=nu.exp(-j[-1])/(alpha[0]*uni_func(age_unq,metal_unq,bins)+alpha[1]*norm_func(j[:-1],mu,sigma))
+    weight_norm=lik[:,-1]/sum(lik[:,-1])
+    #resample
+    
+    #start loop
+    norm_start=int(pop_num*alpha[0])
+    for i in range(10):
+    #calculate new alpha, mu and sigma values
+        temp_phi_norm=nu.zeros(pop_num-norm_start)
+        for j in xrange(pop_num-norm_start):
+            temp_phi_norm[j]=norm_func(lik[norm_start+j,:-1],mu,sigma)
+        temp_alpha=nu.zeros(2)
+        for k in xrange(2):
+            if k==1: #norm dist
+                temp_alpha[k]=sum(weight_norm[norm_start+1:]*alpha[k]*temp_phi_norm/sum(alpha[k]*temp_phi_norm))
+            else: #uniform dist
+                temp_alpha[k]=sum(weight_norm[:norm_start]*alpha[k]*uni_func(age_unq,metal_unq,bins)/(pop_num*alpha[k]**2*uni_func(age_unq,metal_unq,bins)))
+        #make alpha sum to 1
+        temp_alpha=temp_alpha/sum(temp_alpha)
+        mu=nu.sum(nu.tile(weight_norm,(bins*3,1)).T*lik[:,:-1],0)
+        sigma=nu.cov((nu.tile(weight_norm,(bins*3,1)).T*lik[:,:-1]).T)
+        alpha=nu.copy(temp_alpha)
+        #make new points
+        points=pop_builder(pop_num,alpha,mu,sigma,age_unq,metal_unq,bins)
+    #get likelihoods
+        lik=[]
+        pool=Pool()
+        for ii in points:
+            pool.apply_async(like_gen,(data,ii,lib_vals,age_unq,metal_unq,bins,),callback=lik.append)
+        pool.close()
+        pool.join()
+        lik=nu.array(lik)
+
+
+
+def uni_func(age_unq,metal_unq,bins):
+    #calculates the proablility density of uniform function
+    return 1/metal_unq.ptp()**bins*1/(age_unq.ptp()/bins)**bins
+        
+
+def norm_func(x,mu,sigma):
+    #calculates values of normal dist for set of points
+    out=(2*nu.pi)**(-len(mu)/2.)*nu.linalg.det(sigma)**(-.5)
+    out=out*nu.exp(-.5*(nu.dot((x-mu),nu.dot(nu.linalg.inv(sigma),(x-mu).T))))
+    return out
+
+def like_gen(data,active_param,lib_vals,age_unq,metal_unq,bins):
+   #calcs chi squared values
+    model=get_model_fit_opt(active_param,lib_vals,age_unq,metal_unq,bins)  
+    model=data_match_new(data,model,bins)
+    N=[]
+    for k in range(2,len(active_param),3):
+        N.append(active_param[k])
+    N=nu.array(N)
+    model=nu.sum(nu.array(model.values()).T*N,1)
+    #make weight paramer start closer to where ave data value
+    return nu.hstack((active_param,sum((data[:,1]-normalize(data,model)*model)**2)))
+ 
+
+def pop_builder(pop_num,alpha,mu,sigma,age_unq,metal_unq,bins):
+    #creates pop_num of points for evaluation
+    #only uses a multivarate norm and unifor dist for now
+
+    #check if alpha sums to 1
+    if sum(alpha)!=1:
+        alpha=alpha/sum(alpha)
+    #initalize params
+    points=nu.zeros([pop_num,bins*3])
+    #multivariate norm
+    for j in range(mu.shape[0]):
+        #start and stop points
+        if j==0:
+            start=0
+        else:
+            start=stop+1
+        stop=start+int(pop_num*alpha[j])-1
+        points[start:stop,:]=nu.random.multivariate_normal(mu[j],sigma[j],(stop-start)))
+    #check for values outside range
+    bin_index=0
+    for i in range(bins*3):
+        if i==0 or i%3==0: #metalicity
+            index=nu.nonzero(nu.logical_or(points[:,i]< metal_unq[0],points[:,i]> metal_unq[-1]))[0]
+            for j in index:
+                points[j,:]=nu.random.multivariate_normal(mu,sigma)
+                while check(points[j,:],metal_unq, age_unq, bins):
+                    points[j,:]=nu.random.multivariate_normal(mu,sigma)
+        
+        elif (i-1)%3==0 or i-1==0:#age
+             index=nu.nonzero(nu.logical_or(points[:,i]< age_bins[bin_index],points[:,i]>age_bins[bin_index+1]))[0]
+             for j in index:
+                points[j,:]=nu.random.multivariate_normal(mu,sigma)
+                while check(points[j,:],metal_unq, age_unq, bins):
+                    points[j,:]=nu.random.multivariate_normal(mu,sigma)
+        elif (i-2)%3==0 or i==2: #norm
+            index=nu.nonzero(nu.logical_or(points[:,i]< 0,points[:,i]>1))[0]
+            for j in index:
+                points[j,i]=nu.random.rand()
+
+    #normalize normalization parameter
+    if bins>1:
+        temp=nu.zeros(pop_num)
+        for i in range(2,bins*3,3):
+            temp+=points[:,i]
+        for i in range(2,bins*3,3):
+            points[:,i]=points[:,i]/temp
+
+    return points
+
 def MCMC_multi(data,itter,bins,cpus=cpu_count()):
     #more effecent version of multi core MCMC
     #uses cominication methods instead of creating and distroying processes
