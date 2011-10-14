c6924ae7aa1813f6b0f6c2dca204719e70853e66
Thuso Simon <thuso@ast.uct.ac.za>
2011-10-14 07:36:36 +0000

    playing around
    git-svn-id: https://agedate.googlecode.com/svn/ageDate@65 56a2626d-7c62-b8f8-b508-c160e9ae2a0b

--- trunk/Age_date.py
+++ trunk/Age_date.py
@@ -40,7 +40,7 @@ import time as Time
 ###spectral lib stuff####
 global lib_path,spect
 lib_path='/home/thuso/Phd/Spectra_lib/'
-#spect,info= load_spec_lib(lib_path)  
+spect,info= load_spec_lib(lib_path)  
 
 
 def find_az_box(param,age_unq,metal_unq):
