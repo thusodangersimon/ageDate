16501bdcc7ad56b439f3183872442790b22539c5
Thuso Simon <thuso@ast.uct.ac.za>
2011-10-14 16:28:58 +0000

    changed temperature stuff in SA
    git-svn-id: https://agedate.googlecode.com/svn/ageDate@67 56a2626d-7c62-b8f8-b508-c160e9ae2a0b

--- trunk/Age_MCMC.py
+++ trunk/Age_MCMC.py
@@ -67,7 +67,7 @@ def MCMC_SA(data,bins,i,chibest,parambest,option,q=None):
     #does MCMC and reduices the false acceptance rate over a threshold
     #itter needs to be a array of normaly distrbuted numbers
     #so there are no problems with multiprocessing
-
+    print "Starting processor %i" %current_process().ident
     #part on every modual wanting to fit the spectra
     #controls input and expot of files for fitt
     data[:,1]=data[:,1]*1000.  
@@ -98,23 +98,18 @@ def MCMC_SA(data,bins,i,chibest,parambest,option,q=None):
                 active_param[k]=nu.random.random()*age_unq.ptp()+age_unq[0] #random place anywhere
             else: #norm
                 active_param[k]=nu.random.random()*10000
-    #N=sum(active_param.take(range(2,bins*3,3)))
-    #for j in range(2,bins*3,3):            
-    #    active_param[j]=active_param[j]/N
+
     param[0,:]=nu.copy(active_param)
     parambest=nu.copy(active_param)
     chi=nu.zeros(option.itter+1)+nu.inf
-    sigma=nu.identity(len(active_param))*nu.concatenate((nu.tile(
-                [metal_unq.ptp()*nu.random.rand(),age_unq.ptp()/bins*nu.random.rand()],bins),
-                          nu.array([nu.sqrt(bins)]*bins)))
+    sigma=nu.identity(len(active_param))*nu.tile(
+                [0.5,age_unq.ptp()*nu.random.rand(),1.],bins)
 
     model=get_model_fit_opt(active_param,lib_vals,age_unq,metal_unq,bins)  
-    model=data_match_new(data,model,bins)
-    model=nu.sum(nu.array(model.values()).T*active_param.take(range(2,bins*3,3)),1)
-    #make weight paramer start closer to where ave data value
-    #chi[0]=sum((data[:,1]-normalize(data,model)*model)**2)
-    chi[0]=sum((data[:,1]-model)**2)
+    N,model,chi[0]=N_normalize(data, model,bins)
+    active_param[range(2,bins*3,3)]=nu.copy(N)
     chibest.value=chi[0]
+    mybest=nu.copy(chi[0])
     for k in range(len(active_param)):
         parambest[k]=nu.copy(active_param[k])
     #stuff just for age_date
@@ -122,40 +117,39 @@ def MCMC_SA(data,bins,i,chibest,parambest,option,q=None):
     #Nacept,Nreject=nu.ones(len(active_param)),nu.ones(len(active_param))
     Nacept,Nreject,Nexchange_ratio,T_cuurent=1.0,1.0,1.0,0.
     acept_rate,out_sigma=[],[]
-    j,T=1,37450725549.
+    j,T=1,279029.333013
+    T_start,T_stop=-0.34,-1.0
     while option.value and i.value<option.itter:
+        if j%100==0:
+            #print "hi, I'm %i at itter %i and chi %f" %(current_process().ident,j,chi[j-1])
+            #print sigma.diagonal()
+            print Nacept/(Nacept+Nreject)*100.
+            sys.stdout.flush()
         active_param= chain_gen_all(active_param,metal_unq, age_unq,bins,sigma)
-        bin_index=0
-        '''for k in range(1,len(parambest),3):
-            active_param[k]=nu.mean([bin[bin_index],bin[1+bin_index]])
-            bin_index+=1'''
-        #for k in range(0,len(parambest),3):
-        #    active_param[k]=nu.log10(0.0080)
-       #calculate new model and chi
+        #bin_index=0
+      #calculate new model and chi
         model=get_model_fit_opt(active_param,lib_vals,age_unq,metal_unq,bins)  
-        '''N,model=N_normalize(data,model,bins)
-        ii=0
-        for k in range(2,len(parambest),3):
-            active_param[k]=nu.log10(N[ii])
-            ii+=1'''
-        model=data_match_new(data,model,bins)
-        model=nu.sum(nu.array(model.values()).T*active_param.take(range(2,bins*3,3)),1)
-        chi[j]=sum((data[:,1]-normalize(data,model)*model)**2)
+        N,model,chi[j]=N_normalize(data, model,bins)
+        active_param[range(2,bins*3,3)]=nu.copy(N)
         #decide to accept or not
         a=nu.exp((chi[j-1]-chi[j])/2)
         #metropolis hastings
         if a>=1: #acepted
             param[j,:]=nu.copy(active_param)
             Nacept+=1
-            if chi[j]< chibest.value:
-                print 'best fit value %f in iteration %i' %(chi[j],j)
+            if chi[j]< mybest:
+                mybest=nu.copy(chi[j])
+            if chi[j]<chibest.value:
+                print 'best fit value %f in iteration %i, from processor %i' %(chi[j],j,
+                                                                               current_process().ident)
                 sys.stdout.flush()
+                
                 chibest.value=nu.copy(chi[j])
                 for k in range(len(active_param)):
                     parambest[k]=nu.copy(active_param[k])
                 
         else:
-            if nu.exp(nu.log(a)/SA(T_cuurent,option.itter/(cpu),1.5,.11))>nu.random.rand():#false accept
+            if nu.exp(nu.log(a)/SA(T_cuurent,option.itter/(cpu),T_start,T_stop))>nu.random.rand():#false accept
                 param[j,:]=nu.copy(active_param)
                 Nacept+=1
             else:
@@ -164,18 +158,18 @@ def MCMC_SA(data,bins,i,chibest,parambest,option,q=None):
                 chi[j]=nu.copy(chi[j-1])
                 Nreject+=1
  
-        if j<500: #change sigma with acceptance rate
+        if j<1000: #change sigma with acceptance rate
             #k=random.randint(0,len(sigma)-1)
-            if Nacept/Nreject<.50 and all(sigma.diagonal()[:2]>=10**-6): 
+            if Nacept/(Nacept+Nreject)>.50 and all(sigma.diagonal()>=10**-3): 
                #too few aceptnce decrease sigma
                 sigma=sigma/1.05
-            elif Nacept/Nreject>.25: #and all(sigma.diagonal()[:2]<10): #not enough
+            elif Nacept/(Nacept+Nreject)<.25 and all(sigma.diagonal()<10): #not enough
                 sigma=sigma*1.05
         else: #use covarnence matrix
-            if j%500==0:
+            if j%100==0: #and (Nacept/Nreject>.50 or Nacept/Nreject<.25):
                 sigma=Covarence_mat(param,j)
         #change temperature
-        if nu.min([1,nu.exp(-(chi[j-1]-chi[j])/(2.*SA(T_cuurent+1,option.itter/(cpu),1.5,.11))-(chi[j-1]+chi[j])/(2.*SA(T_cuurent,option.itter/(cpu),1.5,.11)))/T])>nu.random.rand():
+        if nu.min([1,nu.exp(-(chi[j-1]-chi[j])/(2.*SA(T_cuurent+1,option.itter/(cpu),T_start,T_stop))-(chi[j-1]+chi[j])/(2.*SA(T_cuurent,option.itter/(cpu),T_start,T_stop)))/T])>nu.random.rand():
             T_cuurent+=1
             Nexchange_ratio+=1   
         #make sure the change temp rate is aroudn 2%
@@ -183,15 +177,27 @@ def MCMC_SA(data,bins,i,chibest,parambest,option,q=None):
             T=T*1.05
         elif Nexchange_ratio/(Nacept+Nreject)<.005:
             T=T/1.05
+        #change temperature schedual
+        if j%50==0:
+            if Nacept/(Nacept+Nreject)>.50:
+                T_start+=.1
+                T_stop+=.1
+            elif Nacept/(Nacept+Nreject)<.25:
+                T_start-=.1
+                T_stop-=.1
+                
 
-        if .01>nu.random.rand(): #every hundred itterations
-            a=nu.exp((chi[j]-chibest.value)/2.0)
+        if .001>nu.random.rand() and j>500: #every hundred itterations
+            a=nu.exp((mybest-chibest.value)/2.0)
             if a>1: #accept change in param
                 #print j
                 chi[j]=nu.copy(chibest.value)
+                mybest=nu.copy(chibest.value)
+                print "swiched places. I'm %i" %current_process().ident
                 for k in range(len(active_param)): 
                     param[j,k]=nu.copy(parambest[k])
                     active_param[k]=nu.copy(parambest[k])
+                    
                 
         j+=1
         i.value=i.value+1
@@ -203,6 +209,7 @@ def MCMC_SA(data,bins,i,chibest,parambest,option,q=None):
     #    param[:,k]=param[:,k]/1000.
     data[:,1]=data[:,1]/1000.
     q.put((param[option.burnin:,:],chi[option.burnin:]))
+   # q.put((param,chi))
     #q.put((param,chi,out_sigma,acept_rate))
     #return param,chi,out_sigma,acept_rate
 
@@ -227,7 +234,7 @@ def outprep(param):
     #changes metals from log to normal
     for i in range(0,param.shape[1],3):
         param[:,i]=10**param[:,i]
-        #param[:,i+2]=param[:,i+2]/10.
+        param[:,i+2]=param[:,i+2]/1000.
         
     return param
 
