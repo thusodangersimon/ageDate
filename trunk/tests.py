549c0077168acff81de288bd81924dfe83397dcf
Thuso Simon <thuso@ast.uct.ac.za>
2011-10-14 07:36:21 +0000

    put a nnls fitting method
    git-svn-id: https://agedate.googlecode.com/svn/ageDate@64 56a2626d-7c62-b8f8-b508-c160e9ae2a0b

--- trunk/tests.py
+++ trunk/tests.py
@@ -29,3 +29,57 @@
 """
 does test on age date program
 """
+
+
+from Age_date import *
+
+def nn_least(data):
+    #uses a non-linear least squares to fit data
+    lam=spect[:,0]
+    index=nu.nonzero(nu.logical_and(lam>=min(data[:,0]),
+                                    lam<=max(data[:,0])))[0]
+    spect=spect[index,1:]
+    N=T.nnls(spect,data[:,1]*1000)[0]
+    current=info[N>0.01]
+    metal,age=[],[]
+    for i in current:
+        metal.append(float(i[4:10]))
+        age.append(float(i[11:-5]))
+    #check if any left
+    if len(current)<2:
+        return float(current[4:10]),float(current[11:-5]),N[N>10**-4]
+
+    return metal[nu.argsort(age)],age[nu.argsort(age)],N[N>10**-4][nu.argsort(age)]/1000.
+
+
+'''
+age1=[]
+for i in info1:
+    age1.append(float(i[11:-5]))
+out=nu.zeros(len(    lam=spect[:,0]
+    index=nu.nonzero(nu.logical_and(lam>=min(data[:,0]),
+                                    lam<=max(data[:,0])))[0]
+    spect=spect[index,1:]
+    N=T.nnls(spect,data[:,1]*1000)[0]
+    current=info[N>0.01]
+    metal,age=[],[]
+    for i in current:
+        metal.append(float(i[4:10]))
+        age.append(float(i[11:-5]))
+    #check if any left
+    if len(current)<2:
+        return float(current[4:10]),float(current[11:-5]),N[N>10**-4]
+
+    return metal[nu.argsort(age)],age[nu.argsort(age)],N[N>10**-4][nu.argsort(age)]/1000.
+
+
+'''
+age1=[]
+for i in info1:
+    age1.append(float(i[11:-5]))
+out=nu.zeros(len(spect[:,0]))
+for i in range(len(N)):
+   out+=N[0][i]*spect[:,i]
+
+
+'''
