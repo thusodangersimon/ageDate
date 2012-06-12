9d6008c18a6f04b1ca625e1ccf0445290a1d03f4
Thuso Simon <drdangersimon@gmail.com>
2012-06-12 16:33:42 +0000

    added new plotting model only works with 3 inputs
    git-svn-id: https://agedate.googlecode.com/svn/ageDate@208 56a2626d-7c62-b8f8-b508-c160e9ae2a0b

--- trunk/visualizations.py
+++ trunk/visualizations.py
@@ -31,17 +31,22 @@ programs to visualize the Age date program
 """
 import Age_date as ag ##temp import##
 import numpy as nu
+from scipy.signal import convolve
+import pylab as lab
 from multiprocessing import Pool,pool
 
-def make_chi_grid(data,dust=None,points=500):
+def make_chi_grid(data,dust=None,points=500, Metal=None, Age=None):
     'makes a 3d pic of metal,age,chi with input spectra 2-D only'
     fun=ag.MC_func(data)
     ag.spect = fun.spect
     #create grid
-    metal,age=nu.meshgrid(nu.linspace(fun._metal_unq.min(),
-                                      fun._metal_unq.max(),points),
-                          nu.linspace(fun._age_unq.min(),
-                                      fun._age_unq.max(),points))
+    if nu.any(Metal) or nu.any(Age):
+        metal,age = nu.meshgrid(Metal,Age)
+    else:
+        metal,age=nu.meshgrid(nu.linspace(fun._metal_unq.min(),
+                                          fun._metal_unq.max(),points),
+                              nu.linspace(fun._age_unq.min(),
+                                          fun._age_unq.max(),points))
 
     param = nu.array(zip(metal.ravel(),age.ravel(),nu.ones_like(age.ravel())))
     #probably need to handel dust in a better way
@@ -59,8 +64,10 @@ def make_chi_grid(data,dust=None,points=500):
     b=nu.array(map(get, out))
     po.close()
     po.join()
-    
-    return metal,age,b[:,-1].reshape(points,points)
+    if nu.any(Metal) or nu.any(Age):
+        return b[:,-1].reshape(points,points)
+    else:
+        return metal,age,b[:,-1].reshape(points,points)
 
 def get(f):
     return f.get()
@@ -72,3 +79,70 @@ def func(data,param,param_dust,lib_vals,age_unq,metal_unq):
     model = ag.dust(nu.hstack((param, param_dust)), model)
     N,chi = ag.N_normalize(data, model, bins)
     return nu.hstack((param, param_dust,chi))
+
+
+def histo_plot(x,y,z=None):
+    '''plots 2-d histogram from frequency in poins (x,y)
+    will plot x,y,z pcolor plot with 1 sigma contours if z is given'''
+
+    assert len(x.shape) == 1 and len(y.shape) == 1
+    if nu.any(z):
+        assert z.shape[0] == x.shape[0]
+        
+    #create histogram
+    Z,X,Y = nu.histogram2d(x,y,[200,200])
+    Z = Z.T
+    #get likelhood brute force if z not there
+    if nu.any(z):
+        Zz = lab.griddata(x,y,z,X,Y)
+    else:
+        Zz = make_chi_grid(data, points=100, metal=X, age=Y)
+    #get hist plot ready from pylab docs
+    left, width = 0.1, 0.65
+    bottom, height = 0.1, 0.65
+    bottom_h = left_h = left+width+0.02
+
+    rect_scatter = [left, bottom, width, height]
+    rect_histx = [left, bottom_h, width, 0.2]
+    rect_histy = [left_h, bottom, 0.2, height]
+
+    # start with a rectangular Figure
+    lab.figure(1, figsize=(8,8))
+
+    axMain = lab.axes(rect_scatter)
+    axHistx = lab.axes(rect_histx)
+    axHisty = lab.axes(rect_histy)
+    #plot 1 sigma contors
+    axMain.contour(X[3:],Y[3:],blur_image(Z,1),
+                nu.percentile(Z[Z.nonzero()],[16,84]),color=('red','red'))
+    axMain.pcolor(X,Y,Zz,cmap='gray')
+    axHistx.hist(x, bins=200)
+    axHisty.hist(y, bins=200, orientation='horizontal')
+
+    axMain.set_xlabel('Metalicity')
+    axMain.set_ylabel('Age')
+
+    axHistx.set_xlim( axMain.get_xlim() )
+    axHisty.set_ylim( axMain.get_ylim() )
+    lab.show()
+
+#from scipy cookbook
+def gauss_kern(size, sizey=None):
+    """ Returns a normalized 2D gauss kernel array for convolutions """
+    size = int(size)
+    if not sizey:
+        sizey = size
+    else:
+        sizey = int(sizey)
+    x, y = nu.mgrid[-size:size+1, -sizey:sizey+1]
+    g = nu.exp(-(x**2/float(size)+y**2/float(sizey)))
+    return g / g.sum()
+   
+def blur_image(im, n, ny=None) :
+    """ blurs the image by convolving with a gaussian kernel of typical
+    size n. The optional keyword argument ny allows for a different
+    size in the y direction.
+    """
+    g = gauss_kern(n, sizey=ny)
+    improc = convolve(im,g, mode='valid')
+    return improc
