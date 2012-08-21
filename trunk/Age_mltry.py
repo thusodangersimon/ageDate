3e5624483bac4f4528e247c2631c1d1e353c43b9
Thuso Simon <drdangersimon@gmail.com>
2012-08-21 16:06:43 +0000

    making mult-try work
    git-svn-id: https://agedate.googlecode.com/svn/ageDate@217 56a2626d-7c62-b8f8-b508-c160e9ae2a0b

--- trunk/Age_mltry.py
+++ trunk/Age_mltry.py
@@ -30,6 +30,7 @@
 
 import numpy as nu
 import mpi4py.MPI as mpi
+import Age_date as ag
 
 #123456789012345678901234567890123456789012345678901234567890123456789
 def run_multi(data,burnin=5*10**3,k_max=16,option=True,rank=0,q_talk=None,q_final=None,fun=None):
@@ -45,7 +46,7 @@ def run_multi(data,burnin=5*10**3,k_max=16,option=True,rank=0,q_talk=None,q_fina
 
 def root_mc(fun,burnin,itter,k_max):
     #handels root stuff
-
+    print 'Starting head node on %s with rank %i' %(mpi.Get_processor_name(),mpi.COMM_WORLD.Get_rank())
     ####initalize vars and workers
     comm = mpi.COMM_WORLD
     worker_size = mpi.COMM_WORLD
@@ -62,6 +63,8 @@ def root_mc(fun,burnin,itter,k_max):
     #set up LOSVD
     if fun._losvd:
         '''#only gaussian dispersion with no shift for now
+
+
         active_losvd = nu.array([nu.random.rand()*150,0,0,0])
         sigma_losvd = nu.zeros([4,4])
         sigma_losvd[0,0] = nu.random.rand() * 10'''
@@ -159,9 +162,144 @@ def worker_other(fun):
     pass
 
 
-if __name__ == '__main__':
+def mult_single(data, fun, itter=10**5, burnin=5*10**3,  k_max=16):
+    '''test of how multi-try mcmc will work on a single processor'''
+    lib_vals = fun._lib_vals
+    metal_unq = fun._metal_unq
+    age_unq = fun._age_unq
+    #create fun for all number of bins
+    attempt=False
+    param,active_param,chi,sigma={},{},{},{}
+    Nacept,Nreject,acept_rate,out_sigma={},{},{},{}
+    #set up dust if in use
+    if fun._dust:
+        active_dust = nu.random.rand(2)*4.
+        sigma_dust = nu.identity(2)*nu.random.rand()*2
+    else:
+        active_dust = nu.zeros(2)
+        sigma_dust = nu.zeros([2,2])
+    #set up LOSVD
+    if fun._losvd:
+        '''#only gaussian dispersion with no shift for now
+        active_losvd = nu.array([nu.random.rand()*150,0,0,0])
+        sigma_losvd = nu.zeros([4,4])
+        sigma_losvd[0,0] = nu.random.rand() * 10'''
+        active_losvd = nu.random.rand(4)*150
+        sigma_losvd = nu.random.rand(4,4)
+    else:
+        active_losvd = nu.zeros(4)
+        sigma_losvd = nu.zeros([4,4])
+ 
+    bayes_fact={} #to calculate bayes factor
+    #fun=MC_func(data)
+    for i in range(1,k_max+1):
+        param[str(i)]=[]
+        active_param[str(i)],chi[str(i)]=nu.zeros(3*i),[nu.inf]
+        sigma[str(i)]=nu.identity(3*i)*nu.tile(
+            [0.5,age_unq.ptp()*nu.random.rand(),1.],i)
+        #active_dust[str(i)]=nu.random.rand(2)*5.
+        #sigma_dust[str(i)]=nu.identity(2)*nu.random.rand()*2
+        Nacept[str(i)],Nreject[str(i)]=1.,0.
+        acept_rate[str(i)],out_sigma[str(i)]=[.35],[]
+        bayes_fact[str(i)]=[]
+    #bins to start with
+    bins=nu.random.randint(1,k_max)
+    while True:
+    #create starting active params
+        bin=nu.log10(nu.linspace(10**age_unq.min(),10**age_unq.max(),bins+1))
+        bin_index=0
+    #start in random place
+        for k in xrange(3*bins):
+            if any(nu.array(range(0,bins*3,3))==k):#metalicity
+                active_param[str(bins)][k]=(nu.random.random()*metal_unq.ptp()+metal_unq[0])
+            else:#age and normilization
+                if any(nu.array(range(1,bins*3,3))==k): #age
+                    active_param[str(bins)][k]=nu.random.random()*age_unq.ptp()+age_unq[0] #random place anywhere
+                    bin_index+=1
+                else: #norm
+                    active_param[str(bins)][k]=nu.random.random()*10000
+    #try leastquares fit
+        if len(chi[str(bins)])==1:
+            chi[str(bins)].append(0.)
+    #active_param[str(bins)]=fun[str(bins)].n_neg_lest(active_param[str(bins)])
+        (chi[str(bins)][-1],
+         active_param[str(bins)][range(2,bins*3,3)]) = fun.lik(
+            active_param[str(bins)], active_dust, active_losvd)
+    #check if starting off in bad place ie chi=inf or nan
+        if not nu.isfinite(chi[str(bins)][-1]):
+            continue
+        else:
+            break
 
-    import Age_date as ag
+    param[str(bins)].append(nu.copy(nu.hstack((
+                    active_param[str(bins)], active_dust, active_losvd))))
+    #set best chi and param
+    chibest = chi[str(bins)][-1]+.0
+    parambest = nu.hstack((active_param[str(bins)],
+                           active_dust,active_losvd))
+    #start multi-try
+    #number of multi-trys
+    N_try = 100
+    for i in xrange(itter):
+        #generate params
+        send_param = nu.zeros((N_try, bins*3))
+        if fun._dust:
+            send_dust = nu.zeros((N_try, 2))
+        if fun._losvd:
+            send_losvd = nu.zeros((N_try, 4))
+        for ii in xrange(N_try):
+            send_param[ii, :] = fun.proposal(active_param[str(bins)],
+                                             sigma[str(bins)])
+            if fun._dust:
+                send_dust[ii,:]= fun.proposal(active_dust,sigma_dust)
+            if fun._losvd:
+                send_losvd[ii,:]  = fun.proposal(active_losvd, sigma_losvd)
+
+        #calculate chi
+        recv= map(fun.lik, send_param,send_dust,send_losvd)
+        recv_chi, recv_N = nu.zeros(N_try), nu.zeros((N_try,bins))
+        for ii in range(N_try):
+            recv_chi[ii], recv_N[ii,:] = recv[ii]
+        ##select param with best chi square with weighted prob
+        index = nu.isfinite(recv_chi)
+        send_param[:, range(2, bins*3,3)] = recv_N
+        recv_chi = recv_chi[index]
+        send_param = send_param[index]
+        if fun._dust:
+            send_dust = send_dust[index]
+        if fun._losvd:
+            send_losvd = send_losvd[index]
+        weight = nu.exp(-recv_chi/recv_chi.max())
+        index = nu.searchsorted(nu.cumsum(weight)/weight.sum(),nu.random.rand())
+        active_param[str(bins)]  = nu.copy(send_param[index])
+        if fun._dust:
+            active_dust = nu.copy(send_dust[index])
+        if fun._losvd:
+            active_losvd = nu.copy(send_losvd[index])
+        #generate new params
+
+        #calc chi
+
+        #acceptance criteria
+
+        #tune step size
+
+        #store params and chi values
+
+if __name__ == '__main__':
+    comm = mpi.COMM_WORLD
+    if comm.Get_rank() == 0:
+        #print 'Starting head node on %s with rank %i' %(mpi.Get_processor_name(),mpi.COMM_WORLD.Get_rank())
+        dat = nu.random.rand(comm.Get_size())
+        dat = comm.scatter(dat, root=0)
+        print dat
+    else:
+        #print 'Starting worker on %s with rank %i' %(mpi.Get_processor_name(),mpi.COMM_WORLD.Get_rank())
+        dat = None
+        dat = comm.scatter(dat, root=0)
+        print dat
+
+    '''import Age_date as ag
     comm = mpi.COMM_WORLD
     if comm.Get_rank() == 0:
         data,info1,dust,weight = ag.iterp_spec(1,lam_min=2000,lam_max=10000)
@@ -170,7 +308,7 @@ if __name__ == '__main__':
             comm.Send([shape,mpi.INT],dest = i)
             comm.Send([data,mpi.DOUBLE],dest = i)
     else:
-        shape = nu.zeros(2,dtype='int')
+        shape = None #nu.zeros(2,dtype='int')
         comm.Recv(shape,source = 0)
         data = nu.zeros(shape)
         comm.Recv(data,source = 0)
@@ -182,3 +320,4 @@ if __name__ == '__main__':
         root_mc(fun,fun1._burnin,fun1._itter,16)
     else:
            worker_burnin(fun1)                                
+'''
