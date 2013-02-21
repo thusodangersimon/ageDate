781eb5be5853af00bebd78b78fd7f3040144d4d2
Thuso Simon <dr.danger.simon@gmail.com>
2013-02-21 10:04:39 +0000

    Created new toy models for RJMCMC to use. Off by a factor of 2 excpt binomial model. Creating likelyhood models soon!
    git-svn-id: https://agedate.googlecode.com/svn/ageDate@269 56a2626d-7c62-b8f8-b508-c160e9ae2a0b

--- trunk/Gauss_landscape.py
+++ trunk/Gauss_landscape.py
@@ -5,7 +5,7 @@
 # Author: Thuso S Simon
 #
 # Date: 17 of April, 2011
-#TODO: Make init have pror input, 
+#TODO: 
 #
 #    vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
 #    Copyright (C) 2011  Thuso S Simon
@@ -26,212 +26,184 @@
 #
 #
 #
-""" Creates a  N-1 dimensional gaussan landscape for use with a fitting program of user choise the -1 diemsion becames the liklihood. User can input: number of gaussian, possitions and FWHM and the program out puts a N dimesional file repersenting those values. Can also add noise to liklihood. The program will also output total area for prosterior calculations """
+""" creates Likelihood classes for testing of MCMC,RJMCMC and other fitting methods. Also servers as an example on how to write functions needed to run programs"""
 
 _module_name='gauss_param_space'
 
 import numpy as nu
 import pylab as lab
-import mpl_toolkits.mplot3d.axes3d as lab3
-from mpl_toolkits.mplot3d import Axes3D
+#import mpl_toolkits.mplot3d.axes3d as lab3
+#from mpl_toolkits.mplot3d import Axes3D
 import sympy as sy
+import scipy.stats as stat_dist
+from scipy.special import expn
+import rpy2.robjects as ro
 
-class Param_landscape:
-#creates the function for the land scape and makes it callable like a real problem
-    def __init__(self,N=None,num_gaus=None):
-        if N and N>1: #set number of dimensions
-            self._N=N
-        else: #if not set, choose 3 as default
-            self._N=3
-            
-        if num_gaus:#sets number of peaks
-            self._num_gaus=num_gaus
-        else:#default is 1
-            self._num_gaus=1
 
-        #set boudaries where gaussian peaks are located
-        self._bounds=nu.zeros([self._N-1,2])
-        self._mu=nu.zeros([self._N-1,self._num_gaus]) #[dimensions,num of peaks]
-        self._std=nu.zeros([self._N-1,self._num_gaus])
-        self._amp=nu.array([None]*self._num_gaus)
-        for i,j in enumerate(self._bounds[:,0]):
-            self._bounds[i,:]=nu.array([-10,10])
+#template class things needed for programs to run
+class template_class(object):
+    #things needed for mcmc or rjmcmc to run
+    def __init__(self):
+        #initalize and put input data
+        pass
 
-        #set mu of each gausian in N space
-            self._mu[i,:]=nu.diff(self._bounds[i,:])*nu.random.rand(self._num_gaus)+nu.mean(self._bounds[i,0])
-#set sigma for each dimension and correlation
-            self._std[i,:]=5*nu.random.rand(self._num_gaus)
+    def sampler(self):
+        #sampler or proposial distribution for drawing numbers
+        pass
+    
+    def prior(self):
+        #prior, should be a distribution, return values between [0,1] and also returns 0's for out of bounds parameters
+        pass
+    def lik(self):
+        #likelihood calculation, recomended return -2log(lik) or chi squared where smaller values are higher likelihoods
+        #if not using log likelihoods, use higher percicion numbers so values wont go to zero
+        #should not return NaNs if miss calculations should return inf
+        pass
+
+    #stuff for rjmcmc. Not needed for MCMC
+    def birth_function(self):
+        #how to change between models
+        pass
+    
+    #functions to speed up convergence (optional)
+    def partical_swarm(self):
+        #partical swarm, has mpi, multiprocessing or single chain methods
+        pass
+
+    def convergece_test(self):
+        pass
+class Gauss_lik(object):
+    #does N dimensional gaussians with x points
+    
+    def __init__(self):
+        #initalize and put input data
+        self._true_sigma = nu.random.rand() * 50
+        self._true_mu = 0. #nu.random.rand() * 50
+
+    def sampler(self,mu,sigma):
+        #sampler or proposial distribution for drawing numbers
+        return nu.random.multivariate_normal(mu,sigma)
+    
+    def prior(self,x,a=0,b=1):
+        #prior, should be a distribution, return values between [0,1] and also returns 0's for out of bounds parameters
+        #uniform prior with 1 axis only
+        prior_bool = nu.logical_and(x >=a,x<=b)
+        #if true set equal to prior value else make zero
+        out = nu.zeros_like(prior_bool,dtype=float)
+        out[prior_bool] = 1 / float(b - a)
+        return out
         
-        #make gaussian func
-        
 
-#callable fuction the returns likihood of space
+    def lik(self,x):
+        #likelihood calculation, recomended return -2log(lik) or chi squared where smaller values are higher likelihoods
+        #if not using log likelihoods, use higher percicion numbers so values wont go to zero
+        #should not return NaNs if miss calculations should return inf
+        return stat_dist.norm.pdf(x,loc = self._true_mu, scale=self._true_sigma)
 
-    def likeihood_value(self,*args): #input and outputs a single value or vector
-        #make gaussian func
-#no coverance
-        try: 
-            #pass if want to make a [M,N] grid of points
-            out=nu.ones([args[0].shape[0],args[0].shape[1],self._num_gaus])
-            for i,ii in enumerate(args): 
-                for j in range(self._num_gaus): #for each peak run a gauss calc
-                    out[:,:,j]=out[:,:,j]*self.func(ii,self._mu[i,j],self._std[i,j],self._amp[j])
-            return nu.sum(out,axis=2)
-            
-        except AttributeError: #if a single number make 1-d array
-            temp_args=[]
-            for i in args:
-                if i.__class__==[].__class__:
-                    temp_args.append(nu.array(i)) #if  a list
-                else:
-                    temp_args.append(nu.array([i])) #if int 
-            args=temp_args
-           
-        except IndexError: #if float or shape=(N,)
-            if len(args[0].shape)<1:
-                temp_args=[]
-                for i in args:
-                    temp_args.append(nu.array([i]))
-                args=temp_args
 
-        out=nu.ones([args[0].shape[0],self._num_gaus])
+def toyI(p=None,x=115,n=200):
+    '''samples from a binomial distbution with like is binomal and prior is 
+    uniform. n=200, x=115 p(p)=u(0,1) the marginal likelihood should be
+    0.0049753.
+    When called will sample from prior and calc liklihood. If p is given will 
+    cal like of p
+    '''
+    #if no p value sample
+    if not nu.any(p):
+        p = nu.ranom.rand()
+    #calc likelhood
+    
+    return stat_dist.binom.pmf(x,n,p)
 
-        for i,ii in enumerate(args): #if a list turn into an numpy array
-           for j in range(self._num_gaus): #for each peak run a gauss calc
-                out[:,j]=out[:,j]*self.func(ii,self._mu[i,j],self._std[i,j],self._amp[j])
-               
-        return nu.sum(out,axis=1)
+def toyII(p,mu=0.,std=1):
+    '''Test weather a point is drawn from gaussian with unknown mean, and 
+    known std. Has a gausian prior on mean, with mu and std as mean and stadard
+    devation'''
+    return stat_dist.norm.pdf(p,mu,std)
 
-    def plot(self): #plots up to 3-d image of landscape
-        if self._N>3:
-            print 'to many dimesions'
-            rase
-        elif self._N==3: #plot in 3-d
-            fig = lab.figure()
-            ax = Axes3D(fig)
-            #Z=self.likeihood_value(X)+self.likeihood_value(Y)
-            X, Y = nu.meshgrid(nu.arange(self._bounds[0,0],self._bounds[0,1]+0.05 , 0.05),nu.arange(self._bounds[1,0],self._bounds[1,1]+0.05, 0.05) )
-            Z=self.likeihood_value(X,Y)
-            ax.plot_surface(X, Y, nu.log(Z))
-            lab.show()
-            
-        
-    def evidence(self,plot=None):
-        #calculated the evidence or total area for landscape using erf's, uses 
-        #symbolic package to compute integrals
-        num_varable=self._mu.shape[0]
-        #import varible
-        from sympy.abc import x
-        #check for amplitude
-        if all(self._amp):
-            amp=self._std*1.0
-            for i in range(self._std.shape[1]):
-                amp[:,i]=self._amp[i]+amp[:,1]*.0
-        else: #use standar amp for normalization
-            amp=self._std*1.0
-            for i in range(self._std.shape[1]):
-                amp[:,i]=1/(sy.sqrt(2*sy.pi)*amp[:,i])
+def toyIII(x,y,theta,sigma=1,answer=False):
+    '''linear regression model x and y data points. length of theta give 
+    polynomial order. If answer is true, and len(x)<5 will give analytical 
+    answer with theta=[-inf,int] with a normal prior for all theta'''
+    if answer:
+        #calculate analyitical solution
+        return False
+    if len(theta.shape) == 1: #1-d array
+        mu = y - nu.polyval(theta,x)
+        return stat_dist.norm.logpdf(y, nu.polyval(theta,x),sigma)
+    else:
+        mu = nu.zeros((theta.shape[0],len(x)))
+        for i in range(theta.shape[1]):
+            #mu += nu.tile(y,(len(mu),1)) - (nu.tile(theta[:,i],(len(x),1)).T*
+                                            #x**(theta.shape[1] -1-i))
+            mu += nu.tile(theta[:,i],(len(x),1)).T*x**(theta.shape[1] -1-i)
+        return stat_dist.norm.logpdf(nu.tile(y,(len(mu),1)), mu,sigma)
+    #return stat_dist.norm.logpdf(mu,0,sigma)
 
-        ans=sy.Mul(0)
-        #eval integral, multiply same peaks add other peaks
-        for i in range(self._num_gaus):
-            multi_ans=sy.Mul(1.0)
-            
-            for j in range(num_varable):
-                func=str(amp[j,i])+'*sy.exp(-(x-'+str(self._mu[j,i])+')**2/(2*'+str(self._std[j,i])+'**2))'
-                multi_ans=multi_ans*sy.integrate(eval(func),(x,self._bounds[j,0],self._bounds[j,1]))
-            ans=ans+multi_ans
 
-        if plot: #if want plot of function
-            from sympy.abc import x,y
-            main_plot=sy.Mul(0)
-            for i in range(self._num_gaus):
-                multi_ans=sy.Mul(1.0)
-                for j in range(2):
-                    if j==1:
-                        func=str(amp[j,i])+'*sy.exp(-(x-'+str(self._mu[j,i])+')**2/(2*'+str(self._std[j,i])+'**2))'
-                    else: #put in y varible
-                        func=str(amp[j,i])+'*sy.exp(-(y-'+str(self._mu[j,i])+')**2/(2*'+str(self._std[j,i])+'**2))'
+def sample(x, size, replace=False, prob=None):
+    '''like R package sample. Samples from x with prob of prob, can be any size
+    and with or without replacement. Not complete yet, only does sampling with replacement with prob vector'''
+    #set up for R
+    r_x = ro.Vector(list(x))
+    r_size = ro.Vector(size)
+    r_replace = ro.Vector(replace)
+    r_prob = ro.Vector(list(prob))
+    r_sample = ro.r.sample(r_x,r_size,r_replace,r_prob)
+    out =[]
+    for i in r_sample:
+        out.append(i.__getitem__(0))
+    return nu.array(out)
 
-                    multi_ans=multi_ans*sy.Mul(eval(func))
-                main_plot=main_plot+multi_ans
-
-            p=sy.Plot(sy.log(main_plot),[x,self._bounds[0,0],self._bounds[0,1]],[y,self._bounds[1,0],self._bounds[1,1]])
-            p.wait_for_calculations()
-            return float(ans.evalf())/(nu.prod(nu.diff( self._bounds,axis=1))),main_plot
-        else:
-            return float(ans.evalf())/nu.prod(nu.diff( self._bounds,axis=1))
-
-    def func(self,x,mu=0,std=1,amp=None):
-        #calculates gaussian function for landscape
-        if amp:
-            return amp*nu.exp(-(x-mu)**2/(2*std**2))
-        else: #noramized to 1 in -inf,inf
-            return 1/(nu.sqrt(2*nu.pi)*std)*nu.exp(-(x-mu)**2/(2*std**2))
-
-################Toy Models##################################################
-import nested_samp as nest
-def toyI(N=300):
-    toyI= Param_landscape()
-    #set 2 d gausian on origen with prior beeing a 2*unit square
-    toyI._mu[0,:]=[0]   
-    toyI._mu[1,:]=[0]
-    toyI._std[0,:]=[1]
-    toyI._std[1,:]=toyI._std[0,:]
-    toyI._bounds[0,:]=[-1.,1.]
-    toyI._bounds[1,:]=[-1.,1.]
-   #run nested sampling on it
-    evidence,prior_vol,evid_error,old_points=nest.nested(toyI,N)
-    lab.plot(prior_vol,nu.log(evidence))
-    lab.title('Toy model 1, 2-D gausian at orgien')
-    lab.xlabel('Prior Volume')
-    lab.ylabel('ln evidence')
-    #compare evidence to analytical one
-    anyilitical=toyI.evidence()
-    print 'Nested program calcuated evidence as %f, anaylitical is %f' %(sum(evidence),anyilitical/4.)
-    lab.show()
+if __name__ == '__main__':
+   #pmc test of calc evidence
+    #with binomial
+    N,K = 542,115 #param for toy
+    n =10**5
+    x = nu.random.rand(n)
+    q = toyI(x,K,N) / stat_dist.uniform.pdf(x)
+    print 'Binomial is estimated %f, real %f'%(nu.mean(q),1/(1.+N))
    
+    #with norm and norm prior (unknow mean, knowns std=prior_std)
+    std = 5.
+    prior_mu = -4
+    x = nu.random.randn(n)
+    q = toyII(x,prior_mu,std)**2 /stat_dist.norm.pdf(x)
+    print 'Normal is estimated %f, real %f'%(nu.mean(q),stat_dist.norm.pdf(prior_mu,0,std))
 
-def toyII(N=300):
-    #3 gausians on a triangle with vertex at (0,-0.22),(-0.25,0.22),(0.25,0.22)
-    #with std=1 and amplitudes of (5,3,9)
-    toyII= Param_landscape(3,3)
-    toyII._mu[0,:]=[0.0,-.25,.25]   
-    toyII._mu[1,:]=[-.22,.22,.22]
-    toyII._std[0,:]=[1.,1.,1.]
-    toyII._std[1,:]=toyII._std[0,:]
-    toyII._amp=[5.,3.,9.]
-    toyII._bounds[0,:]=[-1.,1.]
-    toyII._bounds[1,:]=[-1.,1.]
+    #linear regssion model
+    order = 2 #polynomial order
+    N = 3 #number of point to generate
+    x = nu.linspace(1,5,3)
+    real_m = nu.random.randn(order)
+    y = [  6.33623224,  -4.97248028, -16.18410389]
+    #y = nu.polyval(real_m,x) + nu.random.randn(len(x))
+    #pm
+    X = nu.random.multivariate_normal([0,0],nu.identity(2)*9,n)
+    #cal prior
+    #prior = nu.zeros(n)
+    for i in range(order):
+        if i == 0:
+          prior =  stat_dist.norm.pdf(X[:,i],-3,1)
+        else:
+            prior *= stat_dist.norm.pdf(X[:,i],-3,1)
+    logq = nu.sum(toyIII(x,y,X,2),1) + nu.log(prior) - nu.sum(stat_dist.norm.logpdf(X,0,9),1)
+    print 'Evidence is %.3g' %(nu.exp(logq,dtype=nu.float128).mean())
 
-   #run nested sampling on it
-    evidence,prior_vol,evid_error,old_points=nest.nested(toyII,N)
-    lab.plot(prior_vol,nu.log(evidence))
-    lab.title('Toy model 2, 3, 2-D gausian on Triangle')
-    lab.xlabel('Prior Volume')
-    lab.ylabel('ln evidence')
-    #compare evidence to analytical one
-    anyilitical=toyII.evidence()
-    print 'Nested program calcuated evidence as %f, anaylitical is %f' %(sum(evidence),anyilitical/4.)
-    lab.show()
+    #liklihood plot x=m,y=b,z loglik
+    real_m = nu.array([-3,-3.]) #nu.random.randn(order)*9
+    y = nu.polyval(real_m,x) + nu.random.randn(len(x))
+    M,B = nu.meshgrid(nu.linspace(-50,50,10**2),nu.linspace(-50,50,10**2))
+    X = nu.array(zip(M.ravel(),B.ravel()))
+    for i in range(order):
+        if i == 0:
+          prior =  stat_dist.norm.pdf(X[:,i],-3,1)
+        else:
+            prior *= stat_dist.norm.pdf(X[:,i],-3,1)
 
-
-def toyIII(C=2,N=300):
-    from scipy.special import gamma
-    #Skillin (2004) model where 1 gausian in C dimensions with a
-    #flat prior on a unit circle
-    #Z=(C/2)!*(2*sigma**2)**(C/2)
-    #L=exp(-r**2/(2*sigma**2)) where r**2=sum(theda**2_i,i=0..C)
-    toyIII=Param_landscape(C,1)
-    #set std and amp, and change bounds
-    toyIII._std=nu.zeros([toyIII._N-1,toyIII._num_gaus])+1.0
-    toyIII._amp=nu.array([1.]*C)
-    for i,j in enumerate(toyIII._bounds[:,0]):
-        toyIII._bounds[i,:]=nu.array([-1,1])
-    #set mu's following r<1 and on unit circle
-    while sum (toyIII._mu**2)**.5>1.:
-        toyIII._mu=nu.random.rand(len(toyIII._mu))*2-1
-
-    Z=gamma(C/2.+1)*(2.*toyIII._std[0,0]**2)**(C/2.)
-    evidence,prior_vol,evid_error,old_points=nest.nested(toyIII,N)
-    print 'Nested program calcuated evidence as %f, anaylitical is %f' %(sum(evidence),Z)
+    chi = nu.sum(toyIII(x,y,X),1) + nu.log(prior)
+    print 'Highest likelihood place is at (%1.2f,%1.2f)'%(X[chi.argmax()][0],X[chi.argmax()][1])
+    chi[nu.isinf(chi)] = chi[~nu.isinf(chi)].min()
+    chi = chi.reshape(M.shape)
+    lab.contour(M,B,chi)
+    lab.colorbar()
