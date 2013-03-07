0f599259ac75b3afbaeef312818398b5d2ded304
Thuso Simon <dr.danger.simon@gmail.com>
2013-03-07 12:53:56 +0000

    fixed bug in letting the step size go to inf
    git-svn-id: https://agedate.googlecode.com/svn/ageDate@274 56a2626d-7c62-b8f8-b508-c160e9ae2a0b

--- Other/thuso_quick_fits.py
+++ Other/thuso_quick_fits.py
@@ -128,7 +128,7 @@ def quick_cov_MCMC(x,y,params,func=[],constrants=[],sigma=0.8,itter=10**5,quiet=
     param=nu.array([params,params])#set input parameters [0]=old [1]=new
     if not func: #see if inputed a function
         func=''
-        for i in range(len(params)):
+        for i in xrange(len(params)):
             func=func+'param['+str(i)+']*x**'+str(i)+'+'
         func=func[:-1]
         func=eval('lambda x,param: '+func)
@@ -161,8 +161,8 @@ def quick_cov_MCMC(x,y,params,func=[],constrants=[],sigma=0.8,itter=10**5,quiet=
             print 'current accptence rate %2.2f and chi2 is %2.2f' %(j/(i+1.0)*100.0,chi[1])
            # print param[1] 
  #select new param
-        
         param[1]=nu.random.multivariate_normal(param[0],sigma)
+        
         #check constraints      
         '''if checker:
            for ii in checker:
@@ -214,61 +214,28 @@ def quick_cov_MCMC(x,y,params,func=[],constrants=[],sigma=0.8,itter=10**5,quiet=
         elif j/(i+1.0)<.34 and any(sigma.diagonal()>10**-5) and i<30000: #not enough
            sigma=sigma/5.0
         #change sigma with cov matrix
-        if i>1000 and i%500==0:
-           sigma=nu.cov(out_param[i-1000:i,:].T)
+        if i>1000 and i%500==0 and i<30000:
+           tsigma=nu.cov(out_param[i-1000:i,:].T)
+           if nu.all(nu.isfinite(tsigma)):
+              sigma = tsigma +0.
 
     return out_chi,out_param,parambest,chibest
 
 
 #To run this in ipython, simply copy and paste the lines below into it (excluding the first line)
 if __name__=='__main__':	
- import thuso_quick_fits as T
- #import asciidata
- import numpy as np
- import pylab as pl
- 
- #Enter the time and mag (note it should be in one combined file with 2 columns)
- #targets=asciidata.open('combinednoave.dat')
- temp=nu.loadtxt('combinednoave.dat')
- x=temp[:,0]
- y=temp[:,1]
-
- #x,y=nu.array(x),nu.array(y) 
- func=lambda x,p:p[0]*nu.sin(2*nu.pi*x/0.065714+p[1])+p[2]*nu.sin(2*nu.pi*x/p[3]+p[4])	#The vector p gives the fit parameters - change this to any form that you need.
- param=[1.,0.,1.,0.035,0.]	#Original guess - can be way off - must equal the number of unknowns in the above line
-
- #Now set the limits (this example is for three parameters, lower and upper limits [0,inf]
- const=nu.zeros([5,2])
- const[2,0]=-10
- const[:,1]=const[:,1]+4*nu.pi	#Set the upper limit as infinity and the lower as 0
- #Run the program
- Chi,Param,outparam,outchi=T.quick_cov_MCMC(x,y,param,func,const,itter=3*10**5,sigma=0.02)#,quiet=True)	#When 'quiet' is false, it displays all the guesses
- Chi,Param=np.array(Chi),np.array(Param)
- #####Plot to check fit:
- print 'your best fit parameters are: ',outparam
- print 'your best fit chi squared value is: ', outchi
- #Plot the seperate nights data below one another:
- xplot=[x[0]-np.floor(x[0])];ysineplot=[func(x[0],outparam)];yplot=[y[0]]#;prewhitened=[y[0]-func(x[0],outparam)]	#Plotting vectors
- move = 0	#How much the plot must be moved down (0 for first night, 'move' for next ... )
- for i in range(1,len(x)):	#Split up the plots
-  if int(x[i])-int(x[i-1])<1:
-    xplot.append(x[i]-np.floor(x[i]))	#Modded so that the x-axis starts at 0
-    yplot.append(y[i]+move)
-    ysineplot.append(func(x[i],outparam)+move)
-    #prewhitened.append(y[i]-func(x[i],outparam))
-  else:
-    pl.scatter(xplot,yplot,s=3)
-    pl.plot(xplot,ysineplot)
-    move = move + 5	#Move future plots down by an amount 'move'
-    xplot=[x[i]-np.floor(x[i])];ysineplot=[func(x[i],outparam)+move];yplot=[y[i]+move]	
-    #Restart the lists
-    #prewhitened.append(y[i]-func(x[i],outparam))
- #Comment out if you don't want the prewhitened light curve printed:   
- #for q in range(0,len(x)):
- #  print str(x[q])+' '+str(prewhitened[q])
- #Plot
- pl.scatter(xplot,yplot,s=5)
- pl.plot(xplot,ysineplot)
- yl,yu = pl.ylim()
- pl.ylim(yu,yl)
- pl.show()
+   #import thuso_quick_fits as T
+   #import asciidata
+   import numpy as nu
+   import pylab as lab
+   import Gauss_landscape as G
+   
+   #use quick fits for fit of simple function
+   fun = G.Gaussian_Mixture_model(1000,1)
+   x,y =fun.data[:,0], fun.data[:,1]
+   params = nu.random.randn(2)*9
+   params[1] = nu.abs(params[1])
+   func = lambda x,p: fun.disp_model(p)[1]
+   const = [[-nu.inf,nu.inf],[0,nu.inf]]
+   #start program
+   chi,param,parambest,chibest = quick_cov_MCMC(x,y,params,func,const)
